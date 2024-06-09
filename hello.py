6ffbb27af67455a223b9b439d233c06982d55c77
Gaurav Chetal <chetal.gaurav@gmail.com>
2024-06-09 12:42:10 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    # Declare variables
+    x = 1
+    y = 2
+
+    # Calculate the sum
+    return x + y
+
+
+# Call the function and print the result
+result = add(1,2)
+print("The sum of x and y is:", result)
